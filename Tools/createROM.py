750a4a183c10e1580594854a555ee6a3e94d284e
MrL314 <11429905+MrL314@users.noreply.github.com>
2021-09-10 02:25:00 -0400

    fixed f" issues

--- Tools/createROM.py
+++ Tools/createROM.py
@@ -6,7 +6,7 @@ os.chdir(working_dir)
 print("Assembling .asm files into .rel files!")
 
 def assemble(path):
-    os.system(f"python3 as65c/as65c.py {path} -w -dNES=0 -dJPN=1 -dPAL=0 -dBUG00=0 -dBUG01=0 -dBUG02=0 -dBUG03=0")
+    os.system("python3 as65c/as65c.py {} -w -dNES=0 -dJPN=1 -dPAL=0 -dBUG00=0 -dBUG01=0 -dBUG02=0 -dBUG03=0".format(path))
 
 # SFXINC
 assemble("../VER_0/sfc/ys_main.asm")
@@ -134,7 +134,7 @@ relinfo = relinfo0+" "+relinfo1+" "+relinfo2+" "+relinfo3+" "+relinfo4
 
 link_files = "../VER_0/sfc/ys_main.rel ../VER_0/sfc/ys_save.rel ../VER_0/sfc/ys_title.rel ../VER_0/sfc/ys_enmy.rel ../VER_0/sfc/ys_enmy2.rel ../VER_0/sfc/ys_enmy3.rel ../VER_0/sfc/ys_enmy4.rel ../VER_0/sfc/ys_enmy5.rel ../VER_0/sfc/ys_enmy6.rel ../VER_0/sfc/ys_enmy7.rel ../VER_0/sfc/ys_enmy8.rel ../VER_0/sfc/ys_enmy9.rel ../VER_0/sfc/ys_enmy10.rel ../VER_0/sfc/ys_enmy11.rel ../VER_0/sfc/ys_enmy12.rel ../VER_0/sfc/ys_enmy14.rel ../VER_0/sfc/ys_boss1.rel ../VER_0/sfc/ys_boss2.rel ../VER_0/sfc/ys_bbbros.rel ../VER_0/sfc/ys_koopa.rel ../VER_0/sfc/ys_dorobo.rel ../VER_0/sfc/ys_babym.rel ../VER_0/sfc/ys_enmy13.rel ../VER_0/sfc/ys_exst.rel ../VER_0/sfc/ys_bgsc.rel ../VER_0/sfc/ys_bgsc0.rel ../VER_0/sfc/ys_bgsc1.rel ../VER_0/sfc/ys_bgsc2.rel ../VER_0/sfc/ys_bonus.rel ../VER_0/sfc/ys_start.rel ../VER_0/sfc/ys_mini.rel ../VER_0/sfc/ys_mini1.rel ../VER_0/sfc/ys_ending.rel ../VER_0/sfc/ys_demo.rel ../VER_0/sfc/ys_demo_1.rel ../VER_0/sfc/ys_init.rel ../VER_0/sfc/ys_game.rel ../VER_0/sfc/ys_play.rel ../VER_0/sfc/ys_map.rel ../VER_0/sfc/ys_hmap.rel ../VER_0/sfc/ys_hlder.rel ../VER_0/sfc/ys_rpro.rel ../VER_0/sfc/ys_data.rel ../VER_0/sfc/ys_pldt.rel ../VER_0/sfc/ys_pldt_e.rel ../VER_0/sfc/ys_mapdt.rel ../VER_0/sfc/ys_mpobj.rel ../VER_0/sfc/ys_mpmv.rel ../VER_0/sfc/ys_unit.rel ../VER_0/sfc/ys_w11.rel ../VER_0/sfc/ys_w12.rel ../VER_0/sfc/ys_w13.rel ../VER_0/sfc/ys_w14.rel ../VER_0/sfc/ys_w15.rel ../VER_0/sfc/ys_w16.rel ../VER_0/sfc/ys_w17.rel ../VER_0/sfc/ys_w18.rel ../VER_0/sfc/ys_w19.rel ../VER_0/sfc/ys_w21.rel ../VER_0/sfc/ys_w22.rel ../VER_0/sfc/ys_w23.rel ../VER_0/sfc/ys_w24.rel ../VER_0/sfc/ys_w25.rel ../VER_0/sfc/ys_w26.rel ../VER_0/sfc/ys_w27.rel ../VER_0/sfc/ys_w28.rel ../VER_0/sfc/ys_w29.rel ../VER_0/sfc/ys_w31.rel ../VER_0/sfc/ys_w32.rel ../VER_0/sfc/ys_w33.rel ../VER_0/sfc/ys_w34.rel ../VER_0/sfc/ys_w35.rel ../VER_0/sfc/ys_w36.rel ../VER_0/sfc/ys_w37.rel ../VER_0/sfc/ys_w38.rel ../VER_0/sfc/ys_w39.rel ../VER_0/sfc/ys_w41.rel ../VER_0/sfc/ys_w42.rel ../VER_0/sfc/ys_w43.rel ../VER_0/sfc/ys_w44.rel ../VER_0/sfc/ys_w45.rel ../VER_0/sfc/ys_w46.rel ../VER_0/sfc/ys_w47.rel ../VER_0/sfc/ys_w48.rel ../VER_0/sfc/ys_w49.rel ../VER_0/sfc/ys_w51.rel ../VER_0/sfc/ys_w52.rel ../VER_0/sfc/ys_w53.rel ../VER_0/sfc/ys_w54.rel ../VER_0/sfc/ys_w55.rel ../VER_0/sfc/ys_w56.rel ../VER_0/sfc/ys_w57.rel ../VER_0/sfc/ys_w58.rel ../VER_0/sfc/ys_w59.rel ../VER_0/sfc/ys_w61.rel ../VER_0/sfc/ys_w62.rel ../VER_0/sfc/ys_w63.rel ../VER_0/sfc/ys_w64.rel ../VER_0/sfc/ys_w65.rel ../VER_0/sfc/ys_w66.rel ../VER_0/sfc/ys_w67.rel ../VER_0/sfc/ys_w68.rel ../VER_0/sfc/ys_w69.rel ../VER_0/sfc/ys_w70.rel ../VER_0/sfc/ys_ench.rel ../VER_0/sfc/ys_msgdt.rel ../VER_0/sfc/ys_chr.rel"
 
-os.system(f"python3 link/link.py {link_files} -o ../Output/ys_main.hex {relinfo} -ls ys_main.map")
+os.system("python3 link/link.py {} -o ../Output/ys_main.hex {} -ls ys_main.map".format(link_files, relinfo))
 
 print("Converting hex file")
 os.system("python3 hex2bin/hex2bin.py -m mario -cff -f../Output/ys_main.hex -fys_chip0.hex -i../VER_0/sfc/yschr -ihdr -o../Output/VER_0.sfc -r16")
